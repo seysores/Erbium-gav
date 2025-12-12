725783eb4796fdca868c9675c416bd73f8854d6a
seysores. <seysores@gmail.com>
2025-12-13 00:11:56 +0100

    playlist

--- Erbium/Erbium/Public/Configuration.h
+++ Erbium/Erbium/Public/Configuration.h
@@ -2,19 +2,19 @@
 
 struct FConfiguration
 {
-    static inline auto Playlist = L"/Game/Athena/Playlists/Playlist_DefaultSolo.Playlist_DefaultSolo";
+    static inline auto Playlist = L"/Game/Gav/Levels/GM_1v1/Playlist_DefaultSolo_Respawn.Playlist_DefaultSolo_Respawn";
     static inline auto MaxTickRate = 30.f;
-    static inline auto bLateGame = false;
-    static inline auto LateGameZone = 3; // starting zone
-    static inline auto bLateGameLongZone = false; // zone doesnt close for a long time
+    static inline auto bLateGame = true;
+    static inline auto LateGameZone = 0; // starting zone
+    static inline auto bLateGameLongZone = true; // zone doesnt close for a long time
     static inline auto bEnableCheats = true;
-    static inline auto SiphonAmount = 50; // set to 0 to disable
-    static inline auto bInfiniteMats = false;
-    static inline auto bInfiniteAmmo = false;
+    static inline auto SiphonAmount = 200; // set to 0 to disable
+    static inline auto bInfiniteMats = true;
+    static inline auto bInfiniteAmmo = true;
     static inline auto bForceRespawns = false; // build your client with this too!
-    static inline auto bJoinInProgress = false;
-    static inline auto bAutoRestart = false;
-    static inline auto bKeepInventory = false;
+    static inline auto bJoinInProgress = true;
+    static inline auto bAutoRestart = true;
+    static inline auto bKeepInventory = true;
     static inline auto Port = 7777;
     static inline constexpr auto bEnableIris = true; 
     static inline constexpr auto bGUI = true;
